db14a58132175968d608f17e03df674e5440bcfb
nailBestas <bestasnail@gmailcom>
2025-11-13 19:18:22 +0000

    Go added package main understand

---
+++ GO-Basics/packagemain.go
@@ -0,0 +1,9 @@
+// package main dosya giris cikis icin kullanilir derleyiceye bu dosyadan basla demek
+package main
+
+//fmt format anlamindadir
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
