36ec21b42b5169f8a5c8ba4348a173c107220fa2
nailBestas <bestasnail@gmailcom>
2025-11-17 19:31:35 +0000

    added conditional

---
+++ GO-Basics/11_go_conditional/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	user := "nail"
+	password := "1234"
+	balance := 450.0
+	isBlocked := false
+	roles := []string{"user", "premium"}
+	settings := map[string]bool{"twoFA": true}
+
+	// --- SHORT STATEMENT + IF ---
+	if loginUser := authenticate(user, password); loginUser {
+
+		// --- GUARD PATTERN ---
+		if isBlocked {
+			fmt.Println("Hesabınız bloke edilmiş!")
+			return
+		}
+
+		fmt.Println("Giriş başarılı!")
+
+		// --- IC ICE IF (nested if) ---
+		if settings["twoFA"] {
+			fmt.Println("2FA doğrulandı.")
+
+			if balance > 500 {
+				fmt.Println("Hesap bakiyesi:", balance)
+
+				// --- SLICE içinde arama + IF ---
+				for _, role := range roles {
+					if role == "premium" {
+						fmt.Println("Premium özelliklere erişim açıldı.")
+					}
+				}
+
+			} else {
+				fmt.Println("Bakiye yetersiz!")
+			}
+		}
+
+		// --- ELSE IF ZİNCİRİ ---
+		if balance >= 1000 {
+			fmt.Println("Altın seviye kullanıcı!")
+		} else if balance >= 500 {
+			fmt.Println("Gümüş seviye kullanıcı!")
+		} else if balance >= 100 {
+			fmt.Println("Bronz seviye kullanıcı!")
+		} else {
+			fmt.Println("Yeni kullanıcı!")
+		}
+
+	} else {
+		fmt.Println("Hatalı kullanıcı adı veya şifre!")
+	}
+}
+
+func authenticate(u, p string) bool {
+	return u == "nail" && p == "1234"
+}
