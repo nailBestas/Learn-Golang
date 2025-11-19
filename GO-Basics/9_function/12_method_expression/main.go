cf6a6b6cb862f2b1523c23cc0fe8dbfd174dc871
nailBestas <bestasnail@gmail.com>
2025-11-19 11:00:02 +0000

    methot expression

---
+++ GO-Basics/9_function/12_method_expression/main.go
@@ -0,0 +1,35 @@
+//Bu satır Go programının bir ana paket (main package) olduğunu gösterir.
+
+// main paketi, çalıştırılabilir bir programın başlangıç noktasıdır.
+package main
+
+import (
+	"fmt"
+)
+
+//Burada yeni bir veri tipi (struct) oluşturuyoruz: Dikdortgen.
+// struct birden fazla alanı (property) bir arada tutan bir veri yapısıdır.
+
+// Dikdortgen’in iki alanı var:
+
+// en → dikdörtgenin eni (int tipinde)
+
+// boy → dikdörtgenin boyu (int tipinde)
+type Dikdortgen struct {
+	en, boy int
+}
+
+// method
+// Metod, bir struct ile ilişkili fonksiyondur.
+// Yani normal fonksiyon gibi çalışır ama o struct’a ait bir özellikmiş gibi davranır.
+func (x Dikdortgen) Alan() int {
+	return x.en * x.boy
+}
+
+func main() {
+	d := Dikdortgen{en: 3, boy: 4}
+
+	// Method expression
+	alanFonk := Dikdortgen.Alan
+	fmt.Println(alanFonk(d))
+}
